88d3f650ba0e89eb81ceb261f822cb8e9a86ea11
Javier <jmartin@grupoloyal.com>
2023-06-07 11:48:52 +0200

    Entity

--- curso/Program.cs
+++ curso/Program.cs
@@ -1,11 +1,15 @@
+using curso.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace curso {
     public class Program {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AWContext>(
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddControllersWithViews();
-
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
